9cd0112d0b0986b8e32869f445f150c39c0addbf
fl59762 <62155152+fl59762@users.noreply.github.com>
2021-05-19 12:20:14 -0400

    ppp

---
+++ main.cpp
@@ -0,0 +1,101 @@
+#include "std_lib_facilities.h"
+
+
+int main(){
+    // 3.5 ASSIGNMENT AND INITIALIZATION
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Example 2 3.4 OPERATIONS AND OPERATORS
+    /*
+    cout<<"Please enter two names \n";
+    string first;
+    string second;
+    cin>>first >>second;
+    if (first==second) cout<<"that's the same name twice\n";
+    if (first<second) cout<<first<<" is alphabetically before "<<second<<"\n";
+    if (first>second) cout<<first<<" is alphabetically after "<<second<<"\n";
+    */
+    
+    
+    //TRY THIS 3.4 OPERATIONS AND OPERATORS
+    /*
+    cout<<"please enter a floating-point value: ";
+    int n=0;
+    cin>>n;
+    //double m=n;
+    cout <<"n == "                  <<n
+        <<"\nn+1 == "               <<n+1
+        <<"\nthree times n == "     <<n*3
+        <<"\ntwice n == "           <<n+n
+        <<"\nn squared == "         <<n*n
+        <<"\nhalf of n == "         <<n/2
+        <<"\nsquare root of n == "  <<sqrt(n)
+        <<"\n";
+    */
+    
+    
+    //Example 1 3.4 OPERATIONS AND OPERATORS
+    /*
+    cout<<"type a number followed by a name";
+    
+    int count;
+    cin>>count;
+    string name;
+    cin>>name;
+    
+    int c2 = count+2;
+    string s2=name+" Jr.";
+    
+    int c3 = count-2;
+    
+    cout<<"the original number is "<<count<<"\n";
+    cout<<"the original name is   "<<name<<"\n";
+
+    cout<<s2<<"\n";
+    cout<<c2<<"\n";
+    
+    cout<<c3<<"\n";
+    */
+    
+    
+    //TRY THIS 3.3 INPUT AND TYPE
+    /*
+    cout<<"Please enter yor first name and age\n";
+    string first_name;
+    double age;
+    cin>>first_name;
+    cin>>age;
+    cout<<"Hello " <<first_name <<" (monthes " <<age*12 <<")\n";
+    */
+    
+    
+   //before chapter 3.3
+    /*
+     //i dont remember where this is from
+    int a;
+    int b;
+    int sum;
+    
+    cout<<"Enter a number (press enter after you're done)\n";
+    cin>> a;
+    
+    cout<<"Enter another number (press enter after you're done)\n";
+    cin>> b;
+    
+    sum=a+b;
+    
+    cout<<sum <<endl;
+    */
+    return 0;
+}
+
