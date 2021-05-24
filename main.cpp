b188813f00d6d8fe9ba1bcaea6206f4c08330b20
fl59762 <62155152+fl59762@users.noreply.github.com>
2021-05-24 14:03:14 -0400

    chapter 3 drill

--- main.cpp
+++ main.cpp
@@ -3,11 +3,175 @@
 //g++ -std=c++17 -Wall -Wextra main.cpp
 
 int main(){
-    //3.8 TYPES AND OBJECTS:
     
     
     
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //CHAPTER 3 DRILL
+    /*
+    //Question1
+    
+    string name { };
+    cout<<"\nPlease write the name that you want to write to: ";
+    cin>>name;
+    cout<<"\nDear "<<name<<",\n";
+    
+    
+    //Question2 (continue from Question1)
+     
+    cout<<"\n\tHow are you? I am fine. I miss you. \n";
+    
+    //cout<<"I was hoping you would remember \n"
+    //<<"to give yourself self care because without your best self, you'll feel down. \n"
+    //<<"remember to be patient with yourself";
+    //write the rest later
+    
+    
+    //Question3
+    
+    cout<<"\nWhat's the your friends name? ";
+    string friend_name { };
+    cin>>friend_name;
+    cout<<"\nHave you seen "<<friend_name<<" lately?\n";
+    
+    
+    //Question4
+    
+    char friend_gender {0};
+    cout<<"\nWhat is "<<friend_name<<"'s pronoun? [m=male, f=female] ";
+    cin>>friend_gender;
+    cout<<"\nIf you see "<<friend_name<<", please ask ";
+    if(friend_gender=='m'){
+        cout<<"him";
+    }
+    else if(friend_gender=='f'){
+        cout<<"her";
+    }
+    else {
+        cout<<"them";
+    }
+    cout<<" to call me.\n";
+    
+    
+    //Question5
+    
+    int age;
+    cout<<"\nHow old is "<<name<<"? ";
+    cin>>age;
+    if (0<age&&age<110)
+        cout<<"\nI heard you just had a birthday party and you are "<<age<<" years old.\n";
+    else
+        simple_error("You're kidding!");
+    
+    
+    //Question6
+    
+    if (age<12)
+        cout<<"\nNext year, you will be "<<age+1<<"\n";
+    if (age==17)
+        cout<<"\nNext year, you will be able to vote!\n";
+    if (age>70)
+        cout<<"\nI hope you are enjoying your retirement.\n";
+    
+    
+    //Question7
+    
+    cout<<"\nYours sincerely,\n"
+        <<"\n__________________________\n"
+        <<"__________________________\n"
+        <<"\n"<<name<<"\n";
+    */
+    
+    
+    
+    //3.9.2 UNSAFE CONVERSIONS: Example 2
+    /*
+    double d=0;
+    while (cin>>d){
+        int i{d};
+        //tries to preserve d but int needs to truncate the double so the compiler will make it an error
+        char c=i;
+        int i2=c;
+        cout<<"d=="<<d
+            <<"c=="<<c
+            <<"i2=="<<i2
+            <<"char("<<c<<")\n";
+    }
+    */
+    
+    
+    
+    //3.9.2 UNSAFE CONVERSIONS: TRY THIS
+    /*
+    double d=0;
+    while (cin>>d){
+        int i = d;
+        char c=i;
+        int i2=c;
+        cout<<"d=="<<d
+            <<"c=="<<c
+            <<"i2=="<<i2
+            <<"char("<<c<<")\n";
+    }
+    */
+    
+    
+    
+    //3.9.2 UNSAFE CONVERSIONS: Example 1
+    /*
+    int a=2'000; //if you put 127, it'll print else otherwise 128+ will print oops
+    char c=a;
+    int b=c;
+    if (a != b)
+        cout<<"oops: "<<a<<"!="<<b<<'\n';
+    else
+        cout <<"wow!we have a large characters\n";
+    */
+    
+    
+    
+    //3.9.1 TYPES SAFETY: Example 1
+    /*
+    char c='x';
+    int i1=c;
+    int i2='x';
+    //i1 & i2 will get 120 (integer value of x in character set)
+    char c2=i1;
+    //cout<<c<<'<<i1<<'<<c2<<'\n'; //this is probably outdated as this no longer work
+    cout<<"c = "<<c<<"\ni1 = "<<i1<<"\ni2 = "<<i2<<"\nc2 = "<<c2<<'\n';
+    */
+    
+    //3.9 TYPES SAFETY: Example 1
+    /*
+    double x;
+    double y=x;
+    double z=2.0+x;
+    //since x isnt  even initialized or given any value, the compiler will automatically give you error
+    */
     
     
     
